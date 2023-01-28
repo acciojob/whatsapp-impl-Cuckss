f961d6b6450309409adf92524a4d0cdd2ced69d1
Cuckss <akshatchaturvedi60@gmail.com>
2023-01-28 22:55:18 +0530

    whatsapp

--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -6,5 +6,26 @@ public class Message {
     private int id;
     private String content;
     private Date timestamp;
+    public Message(int id, String content) {
+        this.id = id;
+        this.content = content;
+        this.timestamp= new Date(28-01-2023);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 
 }
